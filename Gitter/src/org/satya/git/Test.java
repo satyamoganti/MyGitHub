b52e44c14dce578085a51abdfd726d4d5dccfd6c
KPMG <KPMG@IN-SATYARAO1>
2014-06-25 19:14:06 +0530

    Testing Remote Git Hub

--- Gitter/src/org/satya/git/Test.java
+++ Gitter/src/org/satya/git/Test.java
@@ -3,8 +3,8 @@ package org.satya.git;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Testing git Project");
+		//Test the remote Git Hub
+		System.out.println("Testing git Project");   
 		// TODO Auto-generated method stub
 
 	}
